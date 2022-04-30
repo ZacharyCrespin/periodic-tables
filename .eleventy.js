75330cf2206a0834872f8a662454b3af2519d450
Zachary C <Zachary.lights@gmail.com>
2022-04-29 17:28:14 -0700

    1.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
 module.exports = function(eleventyConfig) {
 
+    eleventyConfig.addPassthroughCopy('./src/table/*.png');
+    eleventyConfig.addPassthroughCopy('./src/table/*.zip');
+
     eleventyConfig.addPassthroughCopy('./src/images');
-    eleventyConfig.addPassthroughCopy('./src/files');
     eleventyConfig.addPassthroughCopy('./src/css');
+
     eleventyConfig.addPassthroughCopy('./src/404.html');
+    
+    eleventyConfig.addPassthroughCopy('./src/robots.txt');
+    eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
 
     return {
         dir: {
