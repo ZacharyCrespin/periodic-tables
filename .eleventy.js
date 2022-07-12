579803a1eca785e39a9f4598ff4b0563dc6076c7
Zachary Crespin <Zachary.lights@gmail.com>
2022-07-11 20:58:52 -0700

    1.0.5

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./src/table/*.png');
-    eleventyConfig.addPassthroughCopy('./src/table/*.zip');
-
-    eleventyConfig.addPassthroughCopy('./src/images');
     eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/download');
+    eleventyConfig.addPassthroughCopy('./src/images');
 
     eleventyConfig.addPassthroughCopy('./src/404.html');
     
+    eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+
     eleventyConfig.addPassthroughCopy('./src/robots.txt');
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
 
