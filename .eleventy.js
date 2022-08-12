652f54006e80563b5ee37fc257e768384316ce1c
Zachary Crespin <Zachary.lights@gmail.com>
2022-08-12 12:56:07 -0700

    delete remaining files from firebase

--- .eleventy.js
+++ .eleventy.js
@@ -9,8 +9,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css');
     eleventyConfig.addPassthroughCopy('./src/download');
     eleventyConfig.addPassthroughCopy('./src/images');
-
-    eleventyConfig.addPassthroughCopy('./src/404.html');
     
     eleventyConfig.addPassthroughCopy('./src/favicon.ico');
 
