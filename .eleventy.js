79860163f9b56529327be8df46e3c123f4e70b64
Zachary Crespin <Zachary.lights@gmail.com>
2023-06-29 09:56:55 -0700

    passthrough _redirects

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/download");
   eleventyConfig.addPassthroughCopy("./src/images");
 
+  eleventyConfig.addPassthroughCopy("./src/_redirects");
+
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
